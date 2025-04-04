276060a432f7d9c10d3541e980cdb66f57f93e28
Torres <20233tn105@utez.edu.mx>
2025-04-03 20:21:58 -0600

    Correccion login cuentas inactivas

--- src/main/java/com/integradora/AssetTrackerUtez/security/UserDetailsServiceImpl.java
+++ src/main/java/com/integradora/AssetTrackerUtez/security/UserDetailsServiceImpl.java
@@ -28,6 +28,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         Usuario user = usuarioRepository.findFirstByCorreo(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + username));
 
+        //Bloquea el inicio de sesión si el usuario está inactivo
+        if (!user.isEstado()) {
+            throw new RuntimeException("La cuenta del usuario está inactiva");
+        }
         return new org.springframework.security.core.userdetails.User(
                 user.getCorreo(),
                 user.getContrasena(),
